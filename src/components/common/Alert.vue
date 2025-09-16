3bdf67465cc695cc2cd3c572ff1ab0153fb6956f
yongwa1214 <yongwa1214@gmail.com>
2025-09-16 11:17:52 +0900

    모달 메시지 넣기 작업

--- src/components/common/Alert.vue
+++ src/components/common/Alert.vue
@@ -1,10 +1,15 @@
 <script setup>
 import YnModal from './YnModal.vue';
 
+const props = defineProps({
+  content: { type: String, default: "오류 발생, 잠시 후 다시 시도해주십시오." },
+ 
+});
+
 </script>
 
 <template>
-  <YnModal>
+  <YnModal :content="props.content">
     <button class="btn btn-success">확인</button>
   </YnModal>
 </template>
